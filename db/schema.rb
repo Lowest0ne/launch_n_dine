7cc5da65e56ae7a1e1838a6473c2e110a1354cfd
Carl Schwope <schwope.carl@gmail.com>
2013-09-09 15:20:36 -0400

    generate OrderItem model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130909182155) do
+ActiveRecord::Schema.define(version: 20130909192008) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,11 @@ ActiveRecord::Schema.define(version: 20130909182155) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name",       null: false
+  end
+
+  create_table "order_items", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "orders", force: true do |t|
