a058496a4d3bedf81c57bd51125be83781e97966
Carl Schwope <schwope.carl@gmail.com>
2013-09-08 21:21:28 -0400

    Menu has a name

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130909011600) do
+ActiveRecord::Schema.define(version: 20130909011910) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20130909011600) do
   create_table "menus", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name",       null: false
   end
 
   create_table "restaurants", force: true do |t|
