bc6582aacc11c99488d6c5df5bd6f272dd1e5755
Carl Schwope <schwope.carl@gmail.com>
2013-09-09 14:21:17 -0400

    test Order for restaurant_id

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe Order do
-    it "should be tested"
+
+    less_equal_zero = [ 0, -1, -20 ]
+
+    it { should have_valid( :restaurant_id ).when(1) }
+    it { should_not have_valid( :restaurant_id).when( *less_equal_zero ) }
 end
