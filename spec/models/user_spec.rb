66ef86d9b254eddf1ece81c6e4844e63751be5f0
Carl Schwope <schwope.carl@gmail.com>
2013-09-08 21:03:31 -0400

    test User for first and last name

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,32 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+    let ( :user ) { User.new( first_name: "Carl", last_name: "Schwope" ) }
+
+    describe "a valid user" do
+
+        it "should have a first and last name" do
+            expect( user.first_name.length > 0 ).to be_true
+            expect( user.last_name.length    > 0 ).to be_true
+            user.should be_valid
+        end
+    end
+
+    describe "an invalid user" do
+
+        it "should not have a first name" do
+            user.first_name = nil
+            user.should_not be_valid
+            user.first_name = ''
+            user.should_not be_valid
+        end
+
+        it "should not have a last name" do
+            user.last_name = nil
+            user.should_not be_valid
+            user.last_name = ''
+            user.should_not be_valid
+        end
+    end
+
 end
