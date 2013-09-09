21d5b21b8284c290ac26f38045ff4033f41406ae
Carl Schwope <schwope.carl@gmail.com>
2013-09-08 22:41:05 -0400

    annotate MenuItem

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: menu_items
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime
+#  updated_at :datetime
+#  name       :string(255)      not null
+#  price      :float            not null
+#
+
 class MenuItem < ActiveRecord::Base
     validates_presence_of     :name
     validates_numericality_of :price, greater_than_or_equal_to: 0
--- spec/models/menu_item_spec.rb
+++ spec/models/menu_item_spec.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: menu_items
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime
+#  updated_at :datetime
+#  name       :string(255)      not null
+#  price      :float            not null
+#
+
 require 'spec_helper'
 
 describe MenuItem do
