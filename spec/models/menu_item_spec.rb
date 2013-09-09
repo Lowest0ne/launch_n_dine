df3bc9271d5820d8dba6d1013b4db53cfe07783a
Carl Schwope <schwope.carl@gmail.com>
2013-09-08 22:31:44 -0400

    test MenuItem for name and positive price

--- spec/models/menu_item_spec.rb
+++ spec/models/menu_item_spec.rb
@@ -1,5 +1,38 @@
 require 'spec_helper'
 
 describe MenuItem do
-  pending "add some examples to (or delete) #{__FILE__}"
+    let ( :item ) { MenuItem.new( name: "Oatmeal", price: 3.25 ) }
+
+    describe "a valid menu item" do
+
+        it "should have a name and price" do
+            expect( item.name.length > 0).to be_true
+            expect( item.price != nil).to be_true
+        end
+
+        it "should have a positive price" do
+            expect( item.price >= 0 ).to be_true
+            item.should be_valid
+        end
+    end
+
+    describe "an invalid menu item" do
+
+        it "should not have a name" do
+            item.name = nil
+            item.should_not be_valid
+            item.name = ''
+            item.should_not be_valid
+        end
+
+        it "should not have a price" do
+            item.price = nil
+            item.should_not be_valid
+        end
+
+        it "should have a negative price" do
+            item.price = -1
+            item.should_not be_valid
+        end
+    end
 end
