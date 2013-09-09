2d38f5220400b0e2f9c3b0839097383274a2f6bf
Carl Schwope <schwope.carl@gmail.com>
2013-09-09 07:50:03 -0400

    annotate RestaurantOwnership

--- app/models/restaurant_ownership.rb
+++ app/models/restaurant_ownership.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: restaurant_ownerships
+#
+#  id            :integer          not null, primary key
+#  created_at    :datetime
+#  updated_at    :datetime
+#  user_id       :integer          not null
+#  restaurant_id :integer          not null
+#
+# @todo Belongs to User
+# @todo Belongs to Restaurant
+# @note validates presence of user_id, restaurant_id
+
+
 class RestaurantOwnership < ActiveRecord::Base
     validates_presence_of :user_id, :restaurant_id
 end
--- spec/models/restaurant_ownership_spec.rb
+++ spec/models/restaurant_ownership_spec.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: restaurant_ownerships
+#
+#  id            :integer          not null, primary key
+#  created_at    :datetime
+#  updated_at    :datetime
+#  user_id       :integer          not null
+#  restaurant_id :integer          not null
+#
+
 require 'spec_helper'
 
 describe RestaurantOwnership do
