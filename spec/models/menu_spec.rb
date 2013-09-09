7e0708e8ebdbeeadbccb3f196469daf766fd7621
Carl Schwope <schwope.carl@gmail.com>
2013-09-08 21:18:59 -0400

    test Menu for name

--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -1,5 +1,24 @@
 require 'spec_helper'
 
 describe Menu do
-  pending "add some examples to (or delete) #{__FILE__}"
+    let ( :menu ) { Menu.new( name: "Breakfast" ) }
+
+    describe "a valid menu" do
+
+        it "should have a name" do
+            expect( menu.name.length > 0 ).to be_true
+            menu.should be_valid
+        end
+
+    end
+
+    describe "an invalid menu" do
+
+        it "should not have a name" do
+            menu.name = nil
+            menu.should_not be_valid
+            menu.name = ''
+            menu.should_not be_valid
+        end
+    end
 end
