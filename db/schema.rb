8ca2dd61cf0389381996ce060e14e8b923b0d4f2
Carl Schwope <schwope.carl@gmail.com>
2013-09-10 18:20:48 -0400

    generate Restaurant and validate name

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130910212951) do
+ActiveRecord::Schema.define(version: 20130910221555) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "restaurants", force: true do |t|
+    t.string   "name",       null: false
+    t.integer  "user_id",    null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "first_name", null: false
---
+++ spec/models/restaurant_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe Restaurant do
+  it_has_string( :name )
+end
