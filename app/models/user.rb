437e47eec94ee504da13add736a223d991072575
Carl Schwope <schwope.carl@gmail.com>
2013-09-10 18:10:22 -0400

    User valid roles are specific

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ActiveRecord::Base
   validates_presence_of :last_name
   validates_presence_of :role
 
+  ROLES = [ 'driver', 'customer', 'owner' ]
+  def self.valid_roles
+    ROLES
+  end
+
+
+
+  validates_format_of :role, with: /\Adriver\z|\Acustomer\z|\Aowner\z/
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,8 @@ describe User do
 
   it_has_string(:first_name)
   it_has_string(:last_name)
-  it_has_string(:role)
+
+  it { should     have_valid(:role).when( *User.valid_roles ) }
+  it { should_not have_valid(:role).when( nil, '', 'anythingelse', 'drivera', 'adriver' ) }
 
 end
