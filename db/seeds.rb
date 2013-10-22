7c2ab716d7e3e373c58c9fef970b8b677aee9c0b
Carl Schwope <schwope.carl@gmail.com>
2013-10-21 23:39:38 -0400

    create seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'JSON'
+
+seed_data = JSON.parse( File.read( './db/seed_data.json' ), symbolize_names: true)
+
+seed_data[:users].each do |user|
+  if User.where( email: user[:email]).empty?
+    User.create( user )
+  end
+end
+
+seed_data[:restaurants].each do |restaurant|
+  if Restaurant.where( name: restaurant[:name] ).empty?
+    Restaurant.create( restaurant )
+  end
+end
+
+seed_data[:locations].each do |location|
+  if Location.where( street: location[:street] ).empty?
+    Location.create( location )
+  end
+end
+
+seed_data[:menus].each do |menu|
+  if Menu.where( name: menu[:name], restaurant_id: menu[:restaurant_id] ).empty?
+    Menu.create( menu )
+  end
+end
+
+seed_data[:menu_items].each do |menu_item|
+  if MenuItem.where( name: menu_item[:name] ).empty?
+    MenuItem.create( menu_item )
+  end
+end
